2295c80235f79c76d8b3c8a86542cf09bce8fc34
ChloéP <58249770+ChloePottier@users.noreply.github.com>
2020-05-14 15:42:06 +0200

    listing articles, détails articles

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -1,54 +1,46 @@
-<?php/**
+<? php
+/**
  * Content article single file for the Rumble Inn theme
  * @package WordPress
  * @subpackage rumble-inn
  * @since 1.0
  * @version 1.0
- */?>
+ */
+?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		// if ( is_sticky() && is_home() && ! is_paged() ) {
-		// 	printf( '<span class="sticky-post">%s</span>', _x( 'Featured', 'post', 'twentynineteen' ) );
-		// }
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-        endif;
-        
-        ?>
-        
-	</header><!-- .entry-header -->
-
-
-	<div class="entry-content">
+    <div class="entry-header">
         <?php
-        
-		the_content(
-			// sprintf(
-			// 	wp_kses(
-			// 		/* translators: %s: Post title. Only visible to screen readers. */
-			// 		__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentynineteen' ),
-			// 		array(
-			// 			'span' => array(
-			// 				'class' => array(),
-			// 			),
-			// 		)
-			// 	),
-				get_the_title()
-			
-		);
+        if (is_sticky() && is_home() && !is_paged()) {
+            printf('<span class="sticky-post">%s</span>', _x('Featured', 'post', 'rumble-inn'));
+        }
+        if (is_singular()) :
+            the_title('<h1 class="entry-title">', '</h1>');
+        else :
+            the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>');
+        endif;
 
-		// wp_link_pages(
-		// 	array(
-		// 		'before' => '<div class="page-links">' . __( 'Pages:', 'rumble-inn' ),
-		// 		'after'  => '</div>',
-		// 	)
-		// );
-		?>
-	</div><!-- .entry-content -->
+        ?>
 
-	<footer class="entry-footer">
-	</footer><!-- .entry-footer -->
+    </div>
+    <div class="entry-content">
+        <?php
+
+        the_content(
+
+            sprintf(
+                get_the_title()
+            )
+        );
+
+        wp_link_pages(
+            array(
+                'before' => '<div class="page-links">' . __('Pages:', 'rumble-inn'),
+                'after'  => '</div>',
+            )
+        );
+        ?>
+    </div>
+
+    <div class="entry-footer">
+    </div>
 </article><!-- #post-<?php the_ID(); ?> -->
